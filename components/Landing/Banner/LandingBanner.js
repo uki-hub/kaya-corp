be0a41d9b9c328c7a7f1222777b272f500c1e03c
U K I <marzukiaslb@gmail.com>
2022-11-12 09:46:41 +0700

    landing

--- components/Landing/Banner/LandingBanner.js
+++ components/Landing/Banner/LandingBanner.js
@@ -14,26 +14,41 @@ export default function LandingBanner() {
           backgroundImage: `url("${img.default.src}")`,
         }}
       >
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8">
-              <div className="banner-content-wrap">
-                <div
-                  className="banner-btn wow fadeInUp"
-                  data-wow-duration="1.5s"
-                  data-wow-delay="800ms"
-                >
-                  <a href="#ticket-form" className="btn">
-                    Buy Ticket
-                  </a>
-                </div>
-              </div>
+        <a href="#ticket-form" className="buy-ticket">
+          <div
+            style={{
+              position: "absolute",
+              bottom: "5rem",
+              right: 0,
+              left: "0",
+              textAlign: "center",
+            }}
+          >
+            <div
+              href="#ticket-form"
+              style={{
+                backgroundImage: 'url("/assets/images/splash.png")',
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
+                backgroundPosition: "center center",
+              }}
+            >
+              <label
+                style={{
+                  fontSize: "2rem",
+                  height: "6rem",
+                  lineHeight: "6.5rem",
+                  color: "ghostwhite",
+                }}
+              >
+                Buy Ticket
+              </label>
             </div>
           </div>
-        </div>
+        </a>
       </div>
 
-      <div className="tiles"></div>
+      {/* <div className="tiles"></div> */}
     </section>
   );
 }
