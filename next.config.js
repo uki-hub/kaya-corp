0765e1e34c54be532512f06e3307f4a12e7ed227
uki-hub <marzukiaslb@gmail.com>
2022-11-21 14:04:57 +0700

    done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   env: {
     "api-key": "0ed365ed-daf4-4747-a7d8-5434dfbb33c3",
   },
