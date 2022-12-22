75cebfb37e584ee9548abbd4ee6111b73eb3bd61
U K I <marzukiaslb@gmail.com>
2022-12-22 11:51:30 +0700

    gg

--- pages/generate/kartu-peserta.js
+++ pages/generate/kartu-peserta.js
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 const KartuPeserta = ({ data }) => {
   const [style, setStyle] = useState({
     position: "relative",
+    fontFamily: "Josefin Sans",
     width: "1280px",
     height: "527px",
     backgroundImage: `url("data:image/png;base64, ${base64Image}")`,
@@ -29,186 +30,194 @@ const KartuPeserta = ({ data }) => {
       });
   }, []);
 
-  return data.map((d, i) => {
-    if (isMobile)
-      return (
-        <div key={i} style={{ ...style, top: `${i * 1280}px` }}>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "375px",
-              top: "50px",
-              writingMode: "vertical-lr",
-            }}
-          >
-            {d.EBib}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "300px",
-              top: "50px",
-              writingMode: "vertical-lr",
-            }}
-          >
-            {d.nmParticipant}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "145px",
-              top: "50px",
-              writingMode: "vertical-lr",
-            }}
-          >
-            {d.city}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "30px",
-              top: "50px",
-              writingMode: "vertical-lr",
-            }}
-          >
-            {d.brr}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "145px",
-              top: "450px",
-              writingMode: "vertical-lr",
-            }}
-          >
-            {d.dateEvent}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "30px",
-              top: "450px",
-              writingMode: "vertical-lr",
-            }}
-          >
-            {d.timeEvent}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "145px",
-              top: "695px",
-              writingMode: "vertical-lr",
-            }}
-          >
-            {d.gender == "M" ? "Pria" : "Wanita"}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "30px",
-              top: "695px",
-              writingMode: "vertical-lr",
-            }}
-          >
-            {d.jerseySize}
-          </label>
-        </div>
-      );
-    else
-      return (
-        <div key={i} style={style}>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "50px",
-              top: "120px",
-            }}
-          >
-            {d.EBib}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "50px",
-              top: "200px",
-            }}
-          >
-            {d.nmParticipant}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "50px",
-              top: "355px",
-            }}
-          >
-            {d.city}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "50px",
-              top: "465px",
-            }}
-          >
-            {d.brr}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "445px",
-              top: "355px",
-            }}
-          >
-            {d.dateEvent}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "445px",
-              top: "465px",
-            }}
-          >
-            {d.timeEvent}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "690px",
-              top: "355px",
-            }}
-          >
-            {d.gender}
-          </label>
-          <label
-            style={{
-              position: "absolute",
-              fontSize: "30px",
-              left: "690px",
-              top: "465px",
-            }}
-          >
-            {d.jerseySize}
-          </label>
-        </div>
-      );
-  });
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Josefin%20Sans&display=optional"
+      />
+      {data.map((d, i) => {
+        if (isMobile)
+          return (
+            <div key={i} style={{ ...style, top: `${i * 1280}px` }}>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "375px",
+                  top: "50px",
+                  writingMode: "vertical-lr",
+                }}
+              >
+                {d.EBib}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "300px",
+                  top: "50px",
+                  writingMode: "vertical-lr",
+                }}
+              >
+                {d.nmParticipant}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "145px",
+                  top: "50px",
+                  writingMode: "vertical-lr",
+                }}
+              >
+                {d.city}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "30px",
+                  top: "50px",
+                  writingMode: "vertical-lr",
+                }}
+              >
+                {d.brr}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "145px",
+                  top: "450px",
+                  writingMode: "vertical-lr",
+                }}
+              >
+                {d.dateEvent}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "30px",
+                  top: "450px",
+                  writingMode: "vertical-lr",
+                }}
+              >
+                {d.timeEvent}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "145px",
+                  top: "695px",
+                  writingMode: "vertical-lr",
+                }}
+              >
+                {d.gender == "M" ? "Pria" : "Wanita"}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "30px",
+                  top: "695px",
+                  writingMode: "vertical-lr",
+                }}
+              >
+                {d.jerseySize}
+              </label>
+            </div>
+          );
+        else
+          return (
+            <div key={i} style={style}>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "50px",
+                  top: "120px",
+                }}
+              >
+                {d.EBib}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "50px",
+                  top: "200px",
+                }}
+              >
+                {d.nmParticipant}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "50px",
+                  top: "355px",
+                }}
+              >
+                {d.city}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "50px",
+                  top: "465px",
+                }}
+              >
+                {d.brr}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "450px",
+                  top: "355px",
+                }}
+              >
+                {d.dateEvent}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "450px",
+                  top: "465px",
+                }}
+              >
+                {d.timeEvent}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "695px",
+                  top: "355px",
+                }}
+              >
+                {d.gender == "M" ? "Pria" : "Wanita"}
+              </label>
+              <label
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  left: "695px",
+                  top: "465px",
+                }}
+              >
+                {d.jerseySize}
+              </label>
+            </div>
+          );
+      })}
+    </div>
+  );
 };
 
 export default KartuPeserta;
